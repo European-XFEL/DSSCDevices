599b8c4182d31e1a13ff34b7735fe9f2450c3589
Steffen Hauf <steffen.hauf@xfel.eu>
2018-12-11 15:55:59 +0100

    Fix netbeans project

--- nbproject/Karabo-variables-Linux.mk
+++ nbproject/Karabo-variables-Linux.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-Linux
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Debug=libdsscDevices.so
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/libdsscDevices.so
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/libdsscDevices.so
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=libdsscDevices.so.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/libdsscDevices.so.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libdsscDevices.so.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
 CND_ARTIFACT_NAME_Release=libdsscDevices.so
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/libdsscDevices.so
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libdsscDevices.so
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=libdsscDevices.so.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/libdsscDevices.so.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libdsscDevices.so.tar
 #
 # include compiler specific variables
 #
