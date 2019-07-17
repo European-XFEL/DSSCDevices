be54ed83962514b440169ae8b3fabde327f9017e
Andrey Samartsev <samartse@exflqr23726.desy.de>
2019-07-17 20:49:37 +0200

    added reset jtag register in InitSystem dsscPpt,

--- DsscPpt/src/DsscPpt.cc
+++ DsscPpt/src/DsscPpt.cc
@@ -2062,7 +2062,6 @@ namespace karabo {
 
             m_ppt->setGlobalDecCapSetting((SuS::DSSC_PPT::DECCAPSETTING)1);
             
-            //
             std::vector<uint32_t> initial_TempADCvals{4};
             for(uint i=0; i<m_ppt->numAvailableIOBs(); i++){
                 m_ppt->setActiveModule(m_ppt->activeIOBs.at(i));
@@ -2073,7 +2072,6 @@ namespace karabo {
 
             int rc = m_ppt->initSystem();
             
-            //
             for(uint i=0; i<m_ppt->numAvailableIOBs(); i++){
                 m_ppt->setActiveModule(m_ppt->activeIOBs.at(i));
                 m_ppt->jtagRegisters->setSignalValue("Temperature ADC Controller","all","DSTAT",initial_TempADCvals[i]);
