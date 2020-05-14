d3180247755edf83de88ada050761e0e3c15000a
xctrl <xctrl@exflcon145.desy.de>
2020-05-14 15:01:59 +0200

    hutch fixes

--- DsscPpt/src/DsscPpt.hh
+++ DsscPpt/src/DsscPpt.hh
@@ -48,16 +48,11 @@ namespace karabo {
         void trylock(const std::string & info) {
             if (!try_lock()) {
                 std::cout << "---- SmarMutex could not lock mutex at " << info << ". Has been reserved by " << m_origin << std::endl;
-                /*boost::this_thread::sleep_for(boost::chrono::milliseconds(500));
-                if (!try_lock()) {
-                  std::cout << "Second attempt 500 ms later: SmarMutex could not lock mutex at " << info << ". Has been reserved by " << m_origin << std::endl;
-                }//*/
             } else {
                 // this->lock();
             }
             m_origin = info;
         }
-        
         std::string m_origin;
     };
 
