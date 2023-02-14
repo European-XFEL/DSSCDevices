601eee62f1ae8cf4645b7a29ba684e04c6684c3f
Cyril Danilevski <cyril.danilevski@xfel.eu>
2023-02-14 19:39:20 +0100

    Add takeData tests

--- src/DsscControl/take_data.py
+++ src/DsscControl/take_data.py
@@ -4,6 +4,14 @@
 # an extra ~5-10 seconds when using DEPFET (temperature stabilization).
 #
 # If the detector is already acquiring, do NOT stop acquisition before or after the run.
+from asyncio import (
+    CancelledError,
+    create_task,
+    Event,
+    gather,
+)
+from typing import Union
+
 from karabo.middlelayer import (
     Node,
     Slot,
@@ -17,12 +25,30 @@ from karabo.middlelayer import (
     Assignment,
     sleep,
     background,
+    waitUntil,
 )
 
 from .scenes.take_data_scene import take_data_scene
 
 
 class TakeDataNode(Configurable):
+
+    # This event is used to cancel data taking, whether from a slot
+    # or within the device, while respecting the detector state.
+    _event = Event()
+
+    async def _sleep(self, sleep_time: Union[int, float]):
+        # If self._event is set elsewhere, this coro needs
+        # to be cancelled to avoid resetting the event.
+        if sleep_time == 0:
+            return
+        try:
+            await sleep(sleep_time)
+        except CancelledError:
+            pass
+        else:
+            if not self._event.is_set():
+                self._event.set()
 
     @property
     def status(self):
@@ -49,6 +75,7 @@ class TakeDataNode(Configurable):
     
     quantity = UInt32(
         displayedName="Quantity",
+        description="The quantity of trains/secs/mins to take. 0 means continuous",
         defaultValue=20,
         accessMode=AccessMode.RECONFIGURABLE,
         allowedStates={State.ON, State.ACQUIRING},
@@ -68,54 +95,74 @@ class TakeDataNode(Configurable):
         allowedStates={State.ON, State.ACQUIRING},
     )
     async def takeData(self):
+        if self._event.is_set():
+            self.status = "Acquision already ongoing. Cancel first."
+            return  # Some acquisition already in progress
         background(self._take_data)
 
     async def _take_data(self):
-        # Calculate run time, in seconds
-        run_time = self.quantity.value
-
-        if self.unit == "Trains":
-            run_time = run_time / 10
-        if self.unit == "Minutes":
-            run_time *= 60
-        # It's already seconds otherwise.
-
-        msg = f"Will take {run_time} seconds."
-        if self.daqInTheLoop:
-            msg += " With run."
-        self.status = msg
-
-        # Get detector acquiring
+        # Start acquisition
         was_acquiring = self.parent.state == State.ACQUIRING
         if not was_acquiring:
-            self.status = "Make detector acquire."
+            self.status = "Making detector acquire."
             coros = [ppt.runXFEL() for ppt in self.parent.ppt_dev]
             await gather(*coros)
+
             await waitUntil(lambda: self.parent.state == State.ACQUIRING)
-            if self.parent.isDEPFET:
-                self.status = "Waiting for DEPFET to warm up."
-                await sleep(6)  # Let DEPFET sensor warm up.
+
+            # if self.parent.isDEPFET:
+            #     self.status = "Waiting for DEPFET to warm up."
+            #     await sleep(6)  # Let DEPFET sensor warm up.
 
         if self.daqInTheLoop:
             async with getDevice(self.daqController) as controller:
-                controller.start()
+                await controller.start()
                 run_control_id = controller.runController
             async with getDevice(run_control_id) as run_control:
                 self.status = f"Started run #{int(run_control.runNumber)}."
 
-        await sleep(run_time)
+        # Calculate run time in seconds
+        run_time = self.quantity.value
+        if run_time == 0:
+            msg = f"Will take continuously until operator stops."
+        else:
+            if self.unit == "Trains":
+                run_time = run_time / 10
+            if self.unit == "Minutes":
+                run_time *= 60
+            # It's already seconds otherwise.
+            msg = f"Will take {run_time} seconds."
 
         if self.daqInTheLoop:
+            msg += " With run."
+        self.status = msg
+
+        # Take data for the given condition
+        sleep = background(self._sleep(run_time))
+        await self._event.wait()
+        sleep.cancel()  # Should the acquisition be cancelled.
+
+        # Stop acquisition
+        if self.daqInTheLoop:
             async with getDevice(self.daqController) as controller:
-                controller.stop()
+                await controller.stop()
 
         if not was_acquiring:
             coros = [ppt.stopAcquisition() for ppt in self.parent.ppt_dev]
             await gather(*coros)
-            while self.parent.state == State.ACQUIRING:
-                continue
+            await waitUntil(lambda: self.parent.state != State.ACQUIRING)
 
         self.status = "Acquisition done."
+        self._event.clear()
+
+    @Slot(
+        displayedName="Cancel",
+        description="Cancel the data taking",
+        allowedStates=[State.ACQUIRING],
+    )
+    async def cancel(self):
+        self._event.set()
+        self.status = "Acquisition cancelled."
 
 
 class TakeData(Configurable):
--- src/tests/python/test_dssc_control.py
+++ src/tests/python/test_dssc_control.py
@@ -1,8 +1,15 @@
 # Test the DsscControl device.
 import pytest
 
+from asyncio import (
+    create_task,
+    Event,
+    get_running_loop,
+    sleep,
+)
 from DsscControl.dssc_control import DsscControl
-from karabo.middlelayer import Hash
+from karabo.middlelayer import Hash, State
+from karabo.middlelayer_api import device_client
 from karabo.middlelayer_api.eventloop import EventLoop
 
 CONFIG = {
@@ -15,6 +22,28 @@ CONFIG = {
 def event_loop():
     loop = EventLoop()
     yield loop
+
+
+class MockProxy:
+    runNumber = 42
+    runController = "Athens"
+
+    async def start(self):
+        print("starting run")
+
+    async def stop(self):
+        print("stopping run")
+
+
+class MockGetDevice:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    async def __aenter__(self):
+        return MockProxy()
+
+    async def __aexit__(self, *args):
+        pass
 
 
 @pytest.mark.asyncio
@@ -39,3 +68,95 @@ async def test_all_mixins_provide_scene():
     for mixin in mixins:
         res = device.requestScene({"name": mixin})
     assert res["payload"]["name"] == mixin
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "is_depfet, qty, unit, expected_time, ditl",
+    [
+        (False, 40, "Trains",  "Will take 4.0 seconds. With run.", True),
+        (False, 0, "Seconds", "Will take continuously until operator stops. With run.", True),
+    ]
+)
+async def test_take_data_detector_acquiring(is_depfet, qty, unit, expected_time, ditl):
+    device_client._getDevice = MockGetDevice
+
+    config = Hash(CONFIG)
+    config["takeData.daqInTheLoop"] = ditl
+    config["takeData.quantity"] = qty
+    config["takeData.unit"] = unit
+    device = DsscControl(config)
+
+    await device.startInstance()
+    await device.onInitialization()
+
+    device.ppt_dev = []
+
+    # Force using our loop
+    device.takeData._event = Event(loop=get_running_loop())
+
+    assert device.status == "Ready"
+
+    device.state = State.ACQUIRING
+    task = create_task(device.takeData.takeData())
+
+    await sleep(0.1)
+
+    assert device.status == expected_time
+
+    if qty == 0:
+        device.takeData._event.set()
+
+    await sleep(4)
+
+    assert device.status == "Acquisition done."
+    assert task.done()
+    assert not device.takeData._event.is_set()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "is_depfet, qty, unit, expected_time, ditl",
+    [
+        (False, 40, "Trains",  "Will take 4.0 seconds. With run.", True),
+        (False, 0, "Seconds", "Will take continuously until operator stops. With run.", True),
+    ]
+)
+async def test_take_data_detector_standby(is_depfet, qty, unit, expected_time, ditl):
+    device_client._getDevice = MockGetDevice
+
+    config = Hash(CONFIG)
+    config["isDEPFET"] = is_depfet
+    config["takeData.daqInTheLoop"] = ditl
+    config["takeData.quantity"] = qty
+    config["takeData.unit"] = unit
+    device = DsscControl(config)
+
+    await device.startInstance()
+    await device.onInitialization()
+
+    device.ppt_dev = []
+
+    # Force using our loop
+    device.takeData._event = Event(loop=get_running_loop())
+
+    assert device.status == "Ready"
+
+    create_task(device.takeData._take_data())
+    await sleep(0.1)
+    assert device.status == "Making detector acquire."
+
+    device.state = State.ACQUIRING
+    get_running_loop().something_changed()
+    await sleep(0.1)
+    assert device.status == expected_time
+
+    await sleep(5)  # XXX: do not provide a total run time greater than 4 secs.
+    if qty == 0:
+        device.takeData._event.set()
+    device.state = State.ON
+    get_running_loop().something_changed()
+
+    await sleep(0.1)
+    assert device.status == "Acquisition done."
+    assert not device.takeData._event.is_set()
